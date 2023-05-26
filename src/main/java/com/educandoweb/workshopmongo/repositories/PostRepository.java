a4e4a00f81f62626af85ee8853fd9c88d574e384
zaidancarvalho <felipecfernandez@hotmail.com>
2023-05-26 16:01:27 -0300

    Simple query with query methods

--- src/main/java/com/educandoweb/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/educandoweb/workshopmongo/repositories/PostRepository.java
@@ -1,9 +1,13 @@
 package com.educandoweb.workshopmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.educandoweb.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
